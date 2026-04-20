3e1f95512128fe4641520e3413d62640db121ee7
HALO <halosuke@gmail.com>
2026-04-20 18:23:39 +0900

    ショートカット起動時に言語設定を渡すようにした。

--- tools/make/mod/mod_setup.rb
+++ tools/make/mod/mod_setup.rb
@@ -205,6 +205,8 @@ private
 			file << "\n"
 			file << "!define LANGUAGE\t\t\"#{config['SETTINGS']['language']}\"\n".encode("UTF-8")
 			file << "\n"
+			file << "!define COMMAND_OPTION\t\t\"-lang=#{config['SETTINGS']['language']}\"\n".encode("UTF-8")
+			file << "\n"
 			file << "!define UPDATE_NUMBER\t#{config['UPDATEINFO']['high'].to_i}\n".encode("UTF-8")
 			file << "\n"
 			file << "!define MAJOR_VERSION\t#{@archiver.major_version}\n".encode("UTF-8")
